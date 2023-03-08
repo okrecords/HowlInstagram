596a6672b166085d08b09c6617b3277bf2963a46
okrecords <97423205+okrecords@users.noreply.github.com>
2023-03-08 21:37:43 +0900

    feat : login activity mvvm

--- app/src/main/java/com/okre/howlinstagram/login/FindIdActivity.kt
+++ app/src/main/java/com/okre/howlinstagram/login/FindIdActivity.kt
@@ -1,11 +1,12 @@
-package com.okre.howlinstagram
+package com.okre.howlinstagram.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.okre.howlinstagram.R
 
-class LoginActivity : AppCompatActivity() {
+class FindIdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_find_id)
     }
 }
---
+++ app/src/main/java/com/okre/howlinstagram/login/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.okre.howlinstagram.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.okre.howlinstagram.R
+import com.okre.howlinstagram.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+    val loginViewModel : LoginViewModel by viewModels() // lateinit var loginViewModel: LoginViewModel 과 같다.(androidx.activity를 build.gradle에 implementation 해줘야 사용할 수 있음)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        //loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java] // lateinit을 사용했을 경우,,viewModel이 Activity와 생명주기를 함께 하기 위해서
+        binding.viewModel = loginViewModel // dataBinding data variable viewModel 과 loginViewModel 연결
+        binding.activity = this // dataBinding data variable activity 와 LoginActivity 연결
+        binding.lifecycleOwner = this // binding이 Activity와 생명주기를 함께 하기 위해서
+        setObserve()
+    }
+
+    fun setObserve() {
+        loginViewModel.showInputNumberActivity.observe(this) {
+            if (it) {
+                finish()
+                startActivity(Intent(this, InputNumberActivity::class.java))
+            }
+        }
+        loginViewModel.showFindIdActivity.observe(this) {
+            if (it) {
+                startActivity(Intent(this, FindIdActivity::class.java))
+            }
+        }
+    }
+
+    fun loginEmail() {
+        println("Email")
+        loginViewModel.showInputNumberActivity.value = true
+    }
+
+    fun findId() {
+        println("findId")
+        loginViewModel.showFindIdActivity.value = true
+    }
+}
