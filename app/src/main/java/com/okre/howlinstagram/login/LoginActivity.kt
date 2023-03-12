ef4c5a42bed9b4b4451eb5e5cfa977537cc9261f
okrecords <97423205+okrecords@users.noreply.github.com>
2023-03-12 23:35:18 +0900

    refactor : google login

--- app/src/main/java/com/okre/howlinstagram/login/LoginActivity.kt
+++ app/src/main/java/com/okre/howlinstagram/login/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.okre.howlinstagram.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.okre.howlinstagram.R
 import com.okre.howlinstagram.databinding.ActivityLoginBinding
@@ -15,7 +18,6 @@ class LoginActivity : AppCompatActivity() {
     lateinit var binding : ActivityLoginBinding
     val loginViewModel : LoginViewModel by viewModels() // lateinit var loginViewModel: LoginViewModel 과 같다.(androidx.activity를 build.gradle에 implementation 해줘야 사용할 수 있음)
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
@@ -23,7 +25,6 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel // dataBinding data variable viewModel 과 loginViewModel 연결
         binding.activity = this // dataBinding data variable activity 와 LoginActivity 연결
         binding.lifecycleOwner = this // binding이 Activity와 생명주기를 함께 하기 위해서
-
         setObserve()
     }
 
@@ -40,11 +41,17 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-
-
-
+    
     fun findId() {
         println("findId")
         loginViewModel.showFindIdActivity.value = true
+    }
+
+    // 구글 로그인 성공한 결과 값 받는 함수
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+        loginViewModel.firebaseAuthWithGoogle(account.idToken) // idToken : 로그인한 사용자 정보를 암호화한 값
     }
 }
--- app/src/main/java/com/okre/howlinstagram/login/LoginViewModel.kt
+++ app/src/main/java/com/okre/howlinstagram/login/LoginViewModel.kt
@@ -1,10 +1,18 @@
 package com.okre.howlinstagram.login
 
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.okre.howlinstagram.R
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
     var id : MutableLiveData<String> = MutableLiveData("")
@@ -13,6 +21,19 @@ class LoginViewModel : ViewModel() {
     var showInputNumberActivity : MutableLiveData<Boolean> = MutableLiveData(false)
     var showFindIdActivity : MutableLiveData<Boolean> = MutableLiveData(false)
 
+    // ViewModel -> AndroidViewModel 로 변경하여 context 받아온 이유
+    // ViewModel(Controller)가 View(Activity)를 통제하는 개념을 강조하기 위해 context를 사용
+    var context = getApplication<Application>().applicationContext
+    var googleSignInClient : GoogleSignInClient
+
+    init {
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(context, gso)
+    }
     fun loginWithSignupEmail() { // 화면을 통제하는 Controller 코드는 ViewModel이 가지고 있는 것이 좋음
         println("Email")
         auth.createUserWithEmailAndPassword(id.value.toString(), password.value.toString()) // 꼭 value로 받아야 함
@@ -21,5 +42,19 @@ class LoginViewModel : ViewModel() {
                     showInputNumberActivity.value = true
                 }
             }
+    }
+
+    fun loginGoogle(view: View) {
+        var i = googleSignInClient.signInIntent
+        (view.context as? LoginActivity)?.googleLoginResult?.launch(i)
+    }
+
+    fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                showInputNumberActivity.value = true
+            }
+        }
     }
 }
